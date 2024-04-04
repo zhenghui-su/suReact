b505c7eecd53d7431f3d436757c3c115accaeffc
苏征辉 <343196323@qq.com>
2024-04-04 22:39:29 +0800

    ✨ feat: 增加递归阶段的开始和结束工作

---
+++ packages/react-reconciler/src/beginWork.ts
@@ -0,0 +1,6 @@
+import { FiberNode } from './fiber';
+
+/** 在递归中的递阶段开始工作 */
+export const beginWork = (fiber: FiberNode) => {
+	/** 在这个阶段，我们需要比较当前 FiberNode 和之前的状态，并返回子 FiberNode */
+};
---
+++ packages/react-reconciler/src/completeWork.ts
@@ -0,0 +1,6 @@
+import { FiberNode } from './fiber';
+
+/** 在递归中的归阶段完成工作 */
+export const completeWork = (fiber: FiberNode) => {
+	/** 在这个阶段，完成对当前 FiberNode 的处理，可能包括更新状态、执行副作用等操作 */
+};
